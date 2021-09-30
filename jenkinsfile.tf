eca1b7487fe918940521229d8ab5b7312bf50f10
pushpakarthick <72982022+pushpakarthick@users.noreply.github.com>
2021-09-30 17:56:35 +0530

    Update jenkinsfile.tf

--- jenkinsfile.tf
+++ jenkinsfile.tf
@@ -1,18 +1,18 @@
 pipeline {
     agent 
-stages{
-    stage("Git checkout"){
+stages {
+    stage("Git checkout") {
     steps{
     git branch: "main", credentialsId: "0591e46e-0352-4203-84d5-5c3c1ea534c0", url: "https://github.com/pushpakarthick/test"
     }
 }
 
-   stage("Terraform init"){
+   stage("Terraform init") {
    steps{
      sh "terraform init"
   }
 }
-   stage("Terraform apply"){
+   stage("Terraform apply") {
    steps{
        sh "terraform apply"
    }
